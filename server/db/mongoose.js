d38fed6a4048b13c73dde1a66eacf0d971fb2d93
Talal Talhouk <talal@sbcglobal.net>
2017-05-09 15:25:38 -0700

    add express server and set up post route for todos

---
+++ server/db/mongoose.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+
+module.exports = {mongoose};
+
+
+// examples
+// Model
+
+// var Todo = mongoose.model('Todo', {
+//   text:{
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   },
+//   completed:{
+//     type: Boolean,
+//     default: false
+//   },
+//   completedAt:{
+//     type: Number,
+//     default: null
+//   }
+// })
+// var User = mongoose.model('Users', {
+//   email:{
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   }
+// })
+
+
+// instances
+// var newTodo = new Todo({
+// })
+//
+// newTodo.save()
+//   .then((doc)=>{
+//     console.log("Saved todo");
+//     console.log(doc);
+//   })
+//   .catch((e)=>{
+//     console.log('Unable to save todo');
+//   })
+
+// var newUser = new User({
+//   email: "talal@example.com"
+// })
+// newUser.save()
+//   .then((doc)=>{
+//     console.log("Saved User");
+//     console.log(doc);
+//   })
+//   .catch((e)=>{
+//     console.log('Unable to save User');
+//     console.log(e);
+//   })
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res)=>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save()
+    .then((doc)=>{
+      console.log('Todo Saved');
+      console.log(doc);
+      res.send(doc)
+    })
+    .catch((e)=>{
+      console.log("Todo could not be saved.", e);
+      res.status(400).send(e)
+    })
+})
+
+
+app.listen(3000, ()=>{
+  console.log('listening on port 3000');
+});
