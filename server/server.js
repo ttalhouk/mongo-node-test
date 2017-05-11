0dd6936b91032ca8079fadc896c3dfa1f60a4b26
Talal Talhouk <talal@sbcglobal.net>
2017-05-11 09:39:38 -0700

    add server and test files for post route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {User} = require('./models/user');
 
 var app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res)=>{
   var todo = new Todo({
@@ -16,11 +16,12 @@ app.post('/todos', (req, res)=>{
   todo.save()
     .then((doc)=>{
       console.log('Todo Saved');
-      console.log(doc);
+      //console.log(doc);
       res.send(doc)
     })
     .catch((e)=>{
-      console.log("Todo could not be saved.", e);
+      console.log("Todo could not be saved.");
+      //console.log(e);
       res.status(400).send(e)
     })
 })
@@ -29,3 +30,5 @@ app.post('/todos', (req, res)=>{
 app.listen(3000, ()=>{
   console.log('listening on port 3000');
 });
+
+module.exports = {app};
