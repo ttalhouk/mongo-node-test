15bd974b2a53f60af609369bc117944b46322b5e
Talal Talhouk <talal@sbcglobal.net>
2017-05-15 09:13:35 -0700

    update files for heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
@@ -57,8 +59,8 @@ app.get('/todos/:id', (req,res) => {
 })
 
 
-app.listen(3000, ()=>{
-  console.log('listening on port 3000');
+app.listen(port, ()=>{
+  console.log('listening on port '+ port);
 });
 
 module.exports = {app};
