a2fcfaa18757587af2e2e1d190d38661859a8d54
Talal Talhouk <talal@sbcglobal.net>
2017-05-16 17:09:32 -0700

    refactor config variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,15 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development'){
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-  process.env.PORT =  3000;
-} else if (env === 'test'){
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-  process.env.PORT =  3000;
+if (env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  var envConfig = config[env];
+  Object.keys(envConfig).forEach((key) => process.env[key] = envConfig[key])
 }
+
+// if (env === 'development'){
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+//   process.env.PORT =  3000;
+// } else if (env === 'test'){
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+//   process.env.PORT =  3000;
+// }
